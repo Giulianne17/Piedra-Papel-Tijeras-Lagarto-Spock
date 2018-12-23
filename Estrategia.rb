66c9e70d75c1525aabeace070a2fa396ee6afabd
angelicacosta <14-10005@usb.ve>
2018-12-22 23:06:07 -0400

    Ya esta implementada la clase sesgada

--- Estrategia.rb
+++ Estrategia.rb
@@ -13,9 +13,9 @@ require_relative "Jugada.rb"
 class Estrategia
     attr_reader :nombre, :oponente, :actual, :proxima
     $random = Random.new(42)
-
+    
     def asignarOponente(op)
-        @oponente = op
+        @oponente = op 
     end
     
     #Método to_s que permite mostrar el invocante como un String.
@@ -116,12 +116,17 @@ class Uniforme < Estrategia
 end
 
 class Sesgada < Estrategia
-    attr_reader :nombre, :movimientos_posibles, :actual, :index
+    attr_reader :nombre, :probabilidades, :total_probabilidades, :actual, :index
     
     #Método constructor
 	def initialize(list)
         @nombre = "Sesgada"
-        @movimientos_posibles = check_list(list)
+        check_list(list.keys)
+        @probabilidades = list
+        @total_probabilidades = 0
+        list.each do |key, value|
+            @total_probabilidades += value
+        end
     end
 
     #Método to_s que permite mostrar el invocante como un String.
@@ -130,19 +135,28 @@ class Sesgada < Estrategia
     end
 
     def prox()
-        @index = $random.rand(0..@movimientos_posibles.length)
-        @actual = @movimientos_posibles[index].to_s
-        if @actual == "Piedra"
-            return Piedra.new()
-        elsif @actual == "Papel"
-            return Papel.new()
-        elsif @actual == "Tijera"
-            return Tijera.new()
-        elsif @actual == "Lagarto"
-            return Lagarto.new()
-        elsif @actual == "Spock"
-            return Spock.new()
+        @index = $random.rand(0..@total_probabilidades)
+        aux1 = @probabilidades.values[0]
+        aux2 = 0
+        list_length = @probabilidades.values.length
+        while (@index > aux1 && aux2 < list_length-1)
+            aux2 += 1
+            aux1 += @probabilidades.values[aux2]
         end
+        aux = @probabilidades.keys[aux2].to_s
+        
+        if aux == "Piedra"
+            @proxima = Piedra.new()
+        elsif aux == "Papel"
+            @proxima = Papel.new()
+        elsif aux == "Tijera"
+            @proxima = Tijera.new()
+        elsif aux == "Lagarto"
+            @proxima = Lagarto.new()
+        elsif aux == "Spock"
+            @proxima = Spock.new()
+        end
+        return @proxima
     end
     
 end
@@ -188,15 +202,15 @@ class Pensar < Estrategia
 
     def prox()
         if @oponente.getActual().is_a?(Piedra)
-            @contadores[:Piedra]= @contadores[:Piedra] + 1
+            @contadores[:Piedra] += 1
         elsif @oponente.getActual().is_a?(Papel)
-            @contadores[:Papel]= @contadores[:Papel] + 1
+            @contadores[:Papel] += 1
         elsif @oponente.getActual().is_a?(Tijera)
-            @contadores[:Tijera]= @contadores[:Tijera] + 1
+            @contadores[:Tijera] += 1
         elsif @oponente.getActual().is_a?(Lagarto)
-            @contadores[:Lagarto]= @contadores[:Lagarto] + 1
+            @contadores[:Lagarto] += 1
         elsif @oponente.getActual().is_a?(Spock)
-            @contadores[:Spock]= @contadores[:Spock] + 1
+            @contadores[:Spock] += 1
         end
 
         suma = 0
@@ -223,6 +237,20 @@ end
 lista = [ :Piedra, :Papel, :Tijera, :Lagarto, :Spock ]
 estra = Uniforme.new(lista)
 puts estra.prox().to_s
+puts estra.prox().to_s
+puts estra.prox().to_s
+puts estra.prox().to_s
+puts estra.prox().to_s
+puts estra.prox().to_s
 
-mapa = { :Piedra => 2, :Papel => 5, :Tijeras => 4, :Lagarto => 3, :Spock => 1}
+mapa = { :Piedra => 2, :Papel => 5, :Tijera => 4, :Lagarto => 3, :Spock => 1}
+sesgadito = Sesgada.new(mapa)
+puts sesgadito.prox().to_s
+puts sesgadito.prox().to_s
+puts sesgadito.prox().to_s
+puts sesgadito.prox().to_s
+puts sesgadito.prox().to_s
+puts sesgadito.prox().to_s
+puts sesgadito.prox().to_s
+puts sesgadito.prox().to_s
 =end
